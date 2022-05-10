9c0df829651961c637887a8adcd4cd6b242457fe
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2022-05-10 22:35:20 +0300

    Product List Arayüzü & İşlevselliği

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { List_Product } from 'src/app/contracts/list_product';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(spinner:NgxSpinnerService,private productService:ProductService,private alertify:AlertifyService) { 
+    super(spinner)
+  }
 
-  ngOnInit(): void {
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
+  dataSource:MatTableDataSource<List_Product> = null;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  async getProducts(){
+    this.showSpinner(SpinnerType.ballnewtoncradle);
+    const allProducts:List_Product[] = await this.productService.read(()=> this.hideSpinner(SpinnerType.ballnewtoncradle),errorMessage => this.alertify.message(errorMessage,{messageType:MessageType.Error,position:Position.TopRight}))
+    this.dataSource = new MatTableDataSource<List_Product>(allProducts);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  async ngOnInit(){
+    await this.getProducts();
   }
 
 }
